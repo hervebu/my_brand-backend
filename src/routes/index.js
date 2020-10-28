f751bceda72b80d2830cf9b55b34fe096b6e977d
hervebu <h.buterak@gmail.com>
2020-10-28 09:49:44 +0200

    fix: import query router and use it to access query routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
-import express from 'express'
 
+import express from 'express'
+import queryRouter from './query.route'
 import articleRouter from './article.route'
 import userRouter from './user.route'
 
@@ -7,6 +8,7 @@ const router = express.Router()
 
 router.use(articleRouter)
 router.use(userRouter)
+router.use(queryRouter)
 
 
 export default router
